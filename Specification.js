67d52db0fdded217da2b7199be17f1dafe48fba7
SoumyajitBasu1988 <soumyajit.basu@srijan.in>
2016-04-22 14:12:45 +0530

    Adding commits for changes in the Specification instance

--- Specification.js
+++ Specification.js
@@ -1,41 +1,62 @@
-//Functionality-1: Check whether proper URL redirection is taking place
-describe('Check URL of the page', function() {
-	browser.ignoreSynchronization = true;
-	it('Should redirect to the proper URL',function(){
-		console.log('executed -- check url redirection');
-		 browser.get('https://aries-dev.herokuapp.com/');
-		 expect(browser.getCurrentUrl()).toEqual('https://aries-dev.herokuapp.com/index.html#/');
-	});
-});
+describe('Home Page',function(){
 
-//Functionality-2: Check whether the title of the page is appearing correct or not
-describe('Check the title of the page', function(){
-	it('Should have a title', function () {
+  beforeEach(function(){
+     browser.ignoreSynchronization = true;
+     browser.get('https://aries-dev.herokuapp.com/');
+     browser.sleep(5000);
+  })
+
+  //Functionality-1: Check whether the URL redirection is proper or not
+  it('Should redirect to the proper URL',function(){
+    console.log('executed -- check url redirection');
+    expect(browser.getCurrentUrl()).toEqual('https://aries-dev.herokuapp.com/index.html#/');
+    browser.sleep(5000);
+  });
+
+  //Functionality-2: Check whether the title of the page is appearing correct or not
+   it('Should have a title', function () {
        browser.ignoreSynchronization = true;
        console.log('executed -- checked title of the page');
-       browser.get('https://aries-dev.herokuapp.com/index.html#/');
-  	   expect(browser.getTitle()).toEqual('WellKept');
+       expect(browser.getTitle()).toEqual('WellKept');
+       browser.sleep(5000);
   });
-});
 
-//Functionality-3: Check whether the SignIn link is present or not
-describe('Check for SignIn link', function(){
-	it('Should have the SignIn href attribute', function(){
-		browser.ignoreSynchronization = true;
-		console.log('executed -- checked attribute sign in');
-		browser.get('https://aries-dev.herokuapp.com/index.html#/');
-		var sign_in = element(by.css('[ng-click="onSignIn()"]'))
-		expect(sign_in.isPresent()).toBeFalsy();
-	});
-});
+  //Functionality-3: Check whether the SignIn link is present or not
+  it('Should have the SignIn href attribute', function(){
+    browser.ignoreSynchronization = true;
+    console.log('executed -- checked attribute sign in');
+    var sign_in = element(by.css('[ng-click="onSignIn()"]'))
+    expect(sign_in.isPresent()).toBe(true);
+    browser.sleep(5000);
+  });
 
-//Functionality-4: Check whether the MyBooking link is present or not
-describe('Check for MyBookings link', function(){
-	it('Should have the MyBooking href attribute', function(){
-		browser.ignoreSynchronization = true;
-		console.log('executed -- checked attribute mybooking');
-		browser.get('https://aries-dev.herokuapp.com/index.html#/');
-		var mybooking = element(by.xpath("//a[contains(text(),'MY BOOKINGS')]"));
-		expect(mybooking.isPresent()).toBeFalsy();
-	});
+  //Functionality-4: Check whether the MyBooking link is present or not
+  it('Should have the MyBooking href attribute', function(){
+    browser.ignoreSynchronization = true;
+    console.log('executed -- checked attribute mybookings');
+    var mybooking = element(by.xpath("//a[contains(text(),'MY BOOKINGS')]"));
+    expect(mybooking.isPresent()).toBe(true);
+    browser.sleep(5000);
+  });
+
+  //Functionality-5: Check whether the Help link is present or not
+  it('Should have the Help href attribute', function(){
+    browser.ignoreSynchronization = true;
+    console.log('executed -- checked attribute help');
+    var help = element(by.xpath("//a[contains(text(),'HELP')]"));
+    expect(help.isPresent()).toBe(true);
+    browser.sleep(5000);
+  });
+
+  //Functionality-6: Login
+  it('Should Login the user', function(){
+    browser.ignoreSynchronization = true;
+    console.log('executed -- Sign in the user');
+    var locator_signin = element(by.css('[ng-click="onSignIn()"]'));
+    browser.sleep(5000);
+    locator_signin.click();
+    browser.sleep(5000);
+  });  
+
+  //End execution
 });
